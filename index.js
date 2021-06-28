d223fdaa92bca1b091a621dea3b327cf03466c62
José Miguel <josleugim@itesm.mx>
2021-06-28 17:02:06 -0500

    Running express

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const app = express();
+
+app.listen({ port: process.env.PORT }, () => {
+    console.log(`🚀 Server ready at ${process.env.HOST}:${process.env.PORT}`)
+});
